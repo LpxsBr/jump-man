be6fe7995eafffa01d7cc51d09f23c0c1208b605
Kleber <anselmolopes.an@gmail.com>
2022-10-30 18:57:58 -0300

    add counter and event jump

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-document.getElementById('hello').innerHTML = 'hello world!'
+
+const man = document.querySelector('.man');
+let time = 0;
+
+
+// counter
+const counter = () => {
+    document.getElementById('count').innerHTML = time;
+    time++
+}
+setInterval(counter, 100)
+
+const jump = () => {
+    man.classList.add('jump')
+    setTimeout(() => {
+        man.classList.remove('jump');
+    }, 500);
+}
+
+document.addEventListener('keydown', jump)
+document.addEventListener('click', jump)
